d116a99b3412af076e2193d783061d4b624c88d1
Fletcher Hart <fletcher.hart@mymail.champlain.edu>
2016-11-30 18:02:49 -0500

    Stuff

---
+++ app/src/main/java/com/fletcherhart/chitchat/ChatFragment.java
@@ -0,0 +1,12 @@
+package com.fletcherhart.chitchat;
+
+import android.support.v4.app.Fragment;
+
+/**
+ * Created by Fletcher on 11/29/2016.
+ */
+
+public class ChatFragment extends Fragment {
+
+
+}
